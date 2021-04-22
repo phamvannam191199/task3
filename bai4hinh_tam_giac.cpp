3b30ab4ca9f5dfe4d0c383a729ece4f6497b4709
phamvannam191199 <82355164+phamvannam191199@users.noreply.github.com>
2021-04-22 18:54:49 +0700

    Add files via upload

---
+++ bai4hinh_tam_giac.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+ int main(){
+ 	
+ 	int x;
+ 	int y;
+ 	int S;
+ 	
+ 	
+ 	
+ 	
+ 	printf("nhap chieu dai cua hight\n", x);
+ 	scanf("%d",x);
+ 	
+ 	
+ 	printf("nhap chieu dai cua bottom\n",y);
+ 	scanf("%d",y);
+ 	
+ 	
+ 	S= x*y/2;
+ 	printf("dien tich tam giac la %d\n",S);
+ 	
+ 	
+ 	
+ 	 return 0;
+ 	
+ 	
+ }
---
+++ dientichtamgiac.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+ int main(){
+ 	
+ 	int x;
+ 	int y;
+ 	int S;
+ 	
+ 	
+ 	
+ 	
+ 	printf("nhap gia tri cua hight\n", x);
+ 	scanf("%d",x);
+ 	
+ 	
+ 	printf("nhap gia tri cua bottom\n",y);
+ 	scanf("%d",y);
+ 	
+ 	
+ 	S= x*y/2;
+ 	printf("dien tich tam giac la %d\n",S);
+ 	
+ 	
+ }
